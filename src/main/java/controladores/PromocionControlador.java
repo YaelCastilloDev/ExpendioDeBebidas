a0fff1bd9e380d150d19984bb9a6aea9c70f9b80
Yael Castillo <yael.castillo.dev@gmail.com>
2025-06-10 18:27:51 -0600

    add: registrar y buscar promociones

--- src/main/java/controladores/PromocionControlador.java
+++ src/main/java/controladores/PromocionControlador.java
@@ -4,10 +4,54 @@
  */
 package controladores;
 
+import modelos.daos.implementaciones.PromocionDAOimpl;
+
+import java.sql.SQLException;
+import java.util.List;
+
 /**
  *
  * @author yael
  */
 public class PromocionControlador {
-    
+
+    private final PromocionDAOimpl promocionDAO = new PromocionDAOimpl();
+
+    public boolean crearPromocionCliente(int idCliente, double porcentaje, String fechaInicio, String fechaFin) {
+        try {
+            return promocionDAO.crearPromocionCliente(idCliente, porcentaje, fechaInicio, fechaFin);
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return false;
+        }
+    }
+
+
+
+    public List<Integer> obtenerPromocionesPorBebida(int idBebida) {
+        try {
+            return promocionDAO.obtenerPromocionesPorBebida(idBebida);
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return List.of();
+        }
+    }
+
+    public List<Integer> obtenerPromocionesPorCliente(int idCliente) {
+        try {
+            return promocionDAO.obtenerPromocionesPorCliente(idCliente);
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return List.of();
+        }
+    }
+
+    public boolean crearPromocionBebida(int idBebida, double porcentaje, String fechaInicio, String fechaFin) {
+        try {
+            return promocionDAO.crearPromocionBebida(idBebida, porcentaje, fechaInicio, fechaFin);
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return false;
+        }
+    }
 }
